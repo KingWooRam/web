70730db2bbabe88bdae4739a94e7bcc1e51dc2c9
Kky-01 <kim010425@naver.com>
2024-12-21 06:33:04 +0900

    Add files via upload

---
+++ src/components/style5051.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+const styles ={
+   Box4: {
+        margin: 16,
+        padding: 16,
+        fontSize: 16,
+        display: "flex",
+        color: "green",
+        flexDirection: "column",
+        
+        
+},
+Box3: {
+    margin: 16,
+    padding: 16,
+    fontSize: 24,
+    display: "flex",
+    color: "black",
+    flexDirection: "column",
+},
+Box2: {
+    margin: 8,
+        padding: 8,
+        fontSize: 24,
+        display: "flex",
+        flexDirection: "row",
+        border: "1px solid grey",
+        borderRadius: 16,
+        alignItems: "center",
+        justifyContent: "center",
+        
+},
+
+Box1: {
+    margin: 8,
+        padding: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        
+        
+},
+dButton: {
+    width: 100, 
+    height: 100, 
+    resizeMode: "contain",
+  },
+  imageContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+}
